ec6c99e81d950e67bc4c4f6da9ae8c91ff6874d1
Rodrigo Kestler <rodrigokestler@gmail.com>
2017-06-20 12:38:58 -0600

    first push

--- js/index.js
+++ js/index.js
@@ -22,6 +22,18 @@ var user = {
 	role:null,
 	estado:null
 };
+var cortina = {
+		cortina: $('#cortina'),
+		container: $('#cortina_container'),
+		show: function(){
+			cortina.container.html(app.loader);
+			cortina.cortina.show();
+		},
+		hide: function(){
+			cortina.container.html('');
+			cortina.cortina.hide();
+		}
+};
 var myModal = {
 		modal : $('#myModal'),
 		modalHeader : $('#myModalHeader'),
@@ -45,7 +57,7 @@ var login = {
             type: 'post',
             timeout: 15000,
             beforeSend: function(){
-            	login.btnForm.loader('disable');
+            	cortina.show();
             },
             error: function(a,b,c){
                 console.log('error '+JSON.stringify(a)+JSON.stringify(b));
@@ -53,7 +65,7 @@ var login = {
             },
             success: function(a){
             	console.log(JSON.stringify(a));
-            	console.log(a.roles[0]);
+            	
             	if(a.msj_error){
             		myModal.open('Oops',a.msj_error);
             	}else{
@@ -61,18 +73,21 @@ var login = {
             		user.email = formData.user_email;
             		user.estado = a.data.estado;
             		user.role = a.roles[0];
+            		window.localStorage.setItem('user',JSON.stringify(a));
+            		login.screen.hide('slide',{direction:'left'},'fast');
+            		/*
             		if(user.estado.toLowerCase()!='pendiente'){
             			login.screen.hide('slide',{direction:'left'},'fast');
             		}else{
-            			myModal.open('Bienvenido','Tu cuenta todavía no ha sido aprobada. Un asesor se estará poniendo en contacto contigo pronto.');
+            			myModal.open('Bienvenido','Tu cuenta todav�a no ha sido aprobada. Un asesor se estar� poniendo en contacto contigo pronto.');
             		}
-            		
+            		*/
             		
             	}
            	    
             },
             complete: function(){ 
-            	login.btnForm.loader('enable',"INICIAR SESION");
+            	cortina.hide();
             }
        });
 	},
@@ -87,6 +102,11 @@ var register = {
 		register: function(formData){
 			formData.action = 'register_user';
 			console.log(JSON.stringify(formData));
+			if(formData.tipo_usuario=='asesor' && $("#telRegister",register.form).val().length!=8){
+				myModal.open('Oops','Debes ingresar un número de teléfono de 8 digitos para poder contactarte. Todos los asesores serán aprobados previamente a utilizar la aplicación')
+				return true;
+			}
+			
 			$.ajax({
 	            url:app.url_ajax,
 	            dataType: 'json',
@@ -109,7 +129,7 @@ var register = {
 	          	    	  .removeAttr('checked')
 	          	    	  .removeAttr('selected');
 	            		register.toggle('hide');
-	            		if(formData.tipo_usario=='cliente'){
+	            		if(formData.tipo_usuario=='cliente'){
 	            			user.pass = formData.user_pass;
 	            			user.email = formData.user_email;
 	            			user.role = 'cliente';
@@ -140,12 +160,27 @@ var register = {
 			}
 		}
 };
+var requerimiento = {
+	requerimientoBtn: $('#agregar_requerimiento'),
+	crearScreen: $('#crear_requerimiento'),
+	toggle:function(tipo){
+		if(tipo=='hide'){
+			requerimiento.crearScreen.hide('slide',{direction:'right'},'fast');
+		}else if(tipo=='show'){
+			requerimiento.crearScreen.show('slide',{direction:'right'},'fast');
+		}
+	},
+	loadEvents: function(){
+		
+	}
+};
 var app = {
     
     url : 'http://megethosinmobiliaria.com/',
     url_ajax : 'http://megethosinmobiliaria.com/wp-admin/admin-ajax.php',
     loader_block: '<div style="display:block;margin:0 auto;width:40px;"><i class="fa fa-cog fa-spin" style="font-size:30px;font-color:black;"></i></div>',
-    loader : '<div style="display:inline-block;margin:0 auto;width:40px;"><i class="fa fa-cog fa-spin" style="font-size:30px;font-color:black;"></i></div>',
+    loader2 : '<div style="display:inline-block;margin:0 auto;width:40px;"><i class="fa fa-cog fa-spin" style="font-size:30px;font-color:black;"></i></div>',
+    loader: '<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>',
     initialize: function() {
         
         //if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -159,7 +194,32 @@ var app = {
         }
     },
     loadEvents: function(){
+    	//Tabs Home
+    	/*
+    	$(".tab-content").on("swiperight",function() {
+    		console.log('swiperight');
+            var $tab = $('#tablist .active').prev();
+            if ($tab.length > 0)
+                $tab.find('a').tab('show');
+        });
+        $(".tab-content").on("swipeleft",function() {
+        	console.log('swipe left');
+            var $tab = $('#tablist .active').next();
+            if ($tab.length > 0)
+                $tab.find('a').tab('show');
+        });
+        */
+    	//General forms
+    	$(document).on('focus','.inputpz',function(){
+    		console.log('focus inputpz');
+    		console.log($(this));
+    		var parent = $(this).closest('.container');
+    		console.log(parent);
+    		parent.scrollTo(this);
+    	});
+    	console.log('load events');
     	//Login Screen
+    	
     	login.form.parsley().on('form:success',function(){
     		var formData = login.form.getFormData();
     		login.login(formData);
